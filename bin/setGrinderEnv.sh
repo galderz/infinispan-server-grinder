2dd057a15eee39add0e48ece9fcbffdc9020f6a5
norman <norman@apache.org>
2012-05-02 09:41:59 +0200

    Check if enviroment vars are already set before fallback to default ones.
    * Make sure we have jboss-logging and everything else in the classpath (by using wildcard entry)
    * Update commons-pool to version 1.6

--- bin/setGrinderEnv.sh
+++ bin/setGrinderEnv.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
-SPYMEMCACHED_HOME=/opt/spymemcached-2.7
-INFINISPAN_HOME=/opt/infinispan
+if [ -z "$SPYMEMCACHED_HOME" ]; then 
+    SPYMEMCACHED_HOME=/opt/spymemcached-2.7
+fi	
 
-GRINDER_HOME=/opt/grinder
+if [ -z "$INFINISPAN_HOME" ]; then 
+    INFINISPAN_HOME=/opt/infinispan
+fi
+	
 
-GRINDER_CP=/Users/g/.m2/repository/log4j/log4j/1.2.16/log4j-1.2.16.jar
+if [ -z "$GRINDER_HOME" ]; then 
+    GRINDER_HOME=/opt/grinder
+fi
+
+echo $GRINDER_HOME
+GRINDER_CP=~/.m2/repository/log4j/log4j/1.2.16/log4j-1.2.16.jar
 GRINDER_CP=$GRINDER_CP:$GRINDER_HOME/lib/grinder.jar
 GRINDER_CP=$GRINDER_CP:$SPYMEMCACHED_HOME/spymemcached.jar
 GRINDER_CP=$GRINDER_CP:$INFINISPAN_HOME/infinispan-core.jar
 GRINDER_CP=$GRINDER_CP:$INFINISPAN_HOME/modules/hotrod-client/infinispan-client-hotrod.jar
-GRINDER_CP=$GRINDER_CP:$INFINISPAN_HOME/modules/hotrod-client/lib/commons-pool-1.5.6.jar
+GRINDER_CP=$GRINDER_CP:$INFINISPAN_HOME/modules/hotrod-client/lib/commons-pool-1.6.jar
 
-GRINDER_PROPERTIES=/Users/g/Go/demos/grinder-infinispan.git/grinder.properties
+# This is needed for logger etc
+GRINDER_CP=$GRINDER_CP:$INFINISPAN_HOME/lib/*
+	
+if [ -z "$GRINDER_PROPERTIES" ]; then 
+    GRINDER_PROPERTIES=/Users/g/Go/demos/grinder-infinispan.git/grinder.properties
+fi
 
 export SERVER_ADDRESS=localhost
